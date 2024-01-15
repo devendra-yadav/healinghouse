e83fe48b1e103d7c89f2de14d73687cd5696c372
Devendra Yadav <devendra_yadav@epam.com>
2024-01-15 13:10:07 +0800

    initial commit

---
+++ src/main/java/com/clinic/healinghouse/HealinghouseApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.healinghouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealinghouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealinghouseApplication.class, args);
+	}
+
+}
