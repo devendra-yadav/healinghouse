1192753a7d9b2a1a2d1f4ad63e71dc3f8dd27c26
Devendra Yadav <devendra.yadav@gmail.com>
2026-07-07 01:25:53 +0800

    syntax issue, comma added in stop script

--- src/main/linux/bin/stop_healinghouse_app.bash
+++ src/main/linux/bin/stop_healinghouse_app.bash
@@ -9,12 +9,12 @@ fi
 
 echo "Going to stop the healing house application for >>$env<< environment";
 
-pid=$(pgrep -f healinghouse.jar --spring.profiles.active=$env)
+pid=$(pgrep -f 'healinghouse.jar --spring.profiles.active=$env')
 
 if [ "a$pid" == "a" ]; then
   echo "Healing house application is not running for >>$env<< environment"
 else
-  pgrep -fa healinghouse.jar --spring.profiles.active=$env;
+  pgrep -fa 'healinghouse.jar --spring.profiles.active=$env';
   echo "Going to kill the process";
   kill -9 $pid;
   ps -elf|grep $pid;
